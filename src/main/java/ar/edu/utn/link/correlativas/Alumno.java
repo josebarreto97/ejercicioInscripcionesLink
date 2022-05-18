33d1a069dc11d0bc3fa47a8601ac0464fb899ecf
Jose Barreto <joseb1997@hotmail.com>
2022-05-18 20:09:32 -0300

    Creación de Clases, Tests y actualización pom.xml

---
+++ src/main/java/ar/edu/utn/link/correlativas/Alumno.java
@@ -0,0 +1,45 @@
+package ar.edu.utn.link.correlativas;
+
+import java.util.Collection;
+import java.util.List;
+
+public class Alumno {
+	
+	private String nombre;
+	private List<Curso> cursos;
+	private Collection<Materia> materiasAprobadas;
+	
+	
+	
+	public Alumno(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public List<Curso> getCursos() {
+		return cursos;
+	}
+	public void setCursos(List<Curso> cursos) {
+		this.cursos = cursos;
+	}
+	public Collection<Materia> getMateriasAprobadas() {
+		return materiasAprobadas;
+	}
+	public void setMateriasAprobadas(Collection<Materia> materiasAprobadas) {
+		this.materiasAprobadas = materiasAprobadas;
+	}
+
+	public void inscribir(Curso curso) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
